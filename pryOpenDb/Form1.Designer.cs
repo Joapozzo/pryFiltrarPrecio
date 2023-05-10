d1d2a49c147e0c0c97d7e126f07b84245b5a558f
root <mgiraudo@ies21.edu.ar>
2023-05-10 13:55:34 -0300

    Agregar archivos de proyecto.

---
+++ pryOpenDb/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace pryOpenDb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.dgvMostrar = new System.Windows.Forms.DataGridView();
+            this.lblFiltrar = new System.Windows.Forms.Label();
+            this.nupFiltrar = new System.Windows.Forms.NumericUpDown();
+            this.c1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMostrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupFiltrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(26, 12);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(270, 73);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(375, 12);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(270, 73);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // dgvMostrar
+            // 
+            this.dgvMostrar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMostrar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.c1,
+            this.c2,
+            this.c3,
+            this.c4});
+            this.dgvMostrar.Location = new System.Drawing.Point(207, 183);
+            this.dgvMostrar.Name = "dgvMostrar";
+            this.dgvMostrar.Size = new System.Drawing.Size(438, 150);
+            this.dgvMostrar.TabIndex = 2;
+            this.dgvMostrar.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // lblFiltrar
+            // 
+            this.lblFiltrar.AutoSize = true;
+            this.lblFiltrar.Location = new System.Drawing.Point(60, 190);
+            this.lblFiltrar.Name = "lblFiltrar";
+            this.lblFiltrar.Size = new System.Drawing.Size(82, 13);
+            this.lblFiltrar.TabIndex = 3;
+            this.lblFiltrar.Text = "Filtrar por precio";
+            // 
+            // nupFiltrar
+            // 
+            this.nupFiltrar.Location = new System.Drawing.Point(63, 206);
+            this.nupFiltrar.Name = "nupFiltrar";
+            this.nupFiltrar.Size = new System.Drawing.Size(120, 20);
+            this.nupFiltrar.TabIndex = 5;
+            // 
+            // c1
+            // 
+            this.c1.HeaderText = "1";
+            this.c1.Name = "c1";
+            // 
+            // c2
+            // 
+            this.c2.HeaderText = "2";
+            this.c2.Name = "c2";
+            // 
+            // c3
+            // 
+            this.c3.HeaderText = "3";
+            this.c3.Name = "c3";
+            // 
+            // c4
+            // 
+            this.c4.HeaderText = "4";
+            this.c4.Name = "c4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 345);
+            this.Controls.Add(this.nupFiltrar);
+            this.Controls.Add(this.lblFiltrar);
+            this.Controls.Add(this.dgvMostrar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMostrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupFiltrar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.DataGridView dgvMostrar;
+        private System.Windows.Forms.Label lblFiltrar;
+        private System.Windows.Forms.NumericUpDown nupFiltrar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c4;
+    }
+}
+
---
+++ pryOpenDb/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+
+
+namespace pryOpenDb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection connection;
+        OleDbCommand command;
+        OleDbDataReader LectorDB;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection = new OleDbConnection();
+                connection.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\VERDULEROS.mdb;";
+                connection.Open();
+                MessageBox.Show("Conexion establecida");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("La Conexion no ha sido establecida" + "\n" + ex );
+                throw;
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float filtrar = Convert.ToSingle(nupFiltrar.Value);
+                command = new OleDbCommand();
+                command.Connection = connection; // CONECTAR EL COMAND CON EL CONNECTION
+                command.CommandType = CommandType.TableDirect;
+
+                // tabla dentro del acces de verduleros, llamada productos
+                command.CommandText = "Productos";
+
+                LectorDB = command.ExecuteReader();
+                while (LectorDB.Read())
+                {
+                    float precioP = Convert.ToSingle(LectorDB[3]);
+                    if (filtrar <= precioP)
+                    {
+                        dgvMostrar.Rows.Add(LectorDB[0], LectorDB[1], LectorDB[2], LectorDB[3]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se ha podido mostrar la tabla" + "\n" + ex);
+            }
+
+        }
+    }
+}
